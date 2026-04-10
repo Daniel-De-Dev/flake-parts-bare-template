ef3cc61ce13fb1245519e75eb7ea8c443e431b8d
Daniel <154839751+Daniel-De-Dev@users.noreply.github.com>
2026-04-10 14:41:13 +0200

    Update shells.nix
    added a reminder message that is echoed when entering shell

--- nix/shells.nix
+++ nix/shells.nix
@@ -6,6 +6,10 @@
       devShells.default = pkgs.mkShell {
         packages = with pkgs; [
         ];
+
+        shellHook = ''
+          echo "Remember to define needed packages and also shell commands to execute to setup environment"
+        '';
       };
     };
 }
